2cf5e5db422d860772d64461a8866d507f03ce86
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-05-01 02:22:00 -0400

    Fix setup sh

--- setup.sh
+++ setup.sh
@@ -2,28 +2,26 @@
 if [ ! -f /usr/bin/mongod ]
   then
     # Mongo Installation
-    sudo apt-get install gnupg
+    apt-get install gnupg
+
     curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-      sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-      --dearmor
+    gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+    --dearmor
 
-    # Use the below command for Ubuntu 20.04 (Focal)
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] \
-      https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | \
-      sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-    # echo "deb [signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] \
-    #     https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | \
-    #     sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org
-    sudo service mongod start
+    apt-get update
+    apt-get install -y mongodb-org
+    # start mongodb from the path
+    /usr/bin/mongod --fork --logpath /var/log/mongodb/mongod.log --config /etc/mongod.conf
 else
   echo "mongo db already installed.  Skipping..."
 fi
 
+echo "mongo db installed"
+
+apt install python3.10-venv -y
 
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip3 install -r requirements.txt
